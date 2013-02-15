e6fd0a544be315a9a17709b938a04239b2101f03
elm <elm@36f187d4-5712-4624-889c-152d48957efa>
2013-02-15 18:11:22 +0000

    Updated the way we track CPU features.  Much more stable now.
    git-svn-id: svn://mamba.eecs.utk.edu/home/plank/svn/Galois-Library@100 36f187d4-5712-4624-889c-152d48957efa

--- whats_my_sse.c
+++ whats_my_sse.c
@@ -4,25 +4,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "intel_cpu_capabilities.h"
+
+struct {
+        unsigned        feature_code;
+        const char *    feature_name;
+} features[] = {
+        {CPU_CAP_MMX, "MMX"},
+        {CPU_CAP_SSE, "SSE"},
+        {CPU_CAP_SSE2, "SSE2"},
+        {CPU_CAP_SSE3, "SSE3"},
+        {CPU_CAP_SSSE3, "SSSE3"},
+        {CPU_CAP_SSE41, "SSE4.1"},
+        {CPU_CAP_SSE42, "SSE4.2"},
+        {CPU_CAP_PCLMULQDQ, "PCLMULQDQ"},
+        {CPU_CAP_AVX, "AVX"},
+        {0, NULL},
+};
+
+
 int main()
 {
-        unsigned int cpeinfo;
-        unsigned int cpsse;
-    asm (
-        "mov $0x1, %%eax\n\t"
-                "cpuid\n\t"
-                "mov %%edx, %0\n\t"
-            "mov %%ecx, %1\n" : "=m" (cpeinfo), "=m" (cpsse)
-                );
+        unsigned        i;
 
-        printf("1 - Instruction set is supported by CPU\n");
-        printf("0 - Instruction set not supported\n"); 
-        printf("--------------------------------\n");
-        printf("MMX:     %d\n", ((cpeinfo >> 23) & 0x1 ));
-        printf("SSE:     %d\n", ((cpeinfo >> 25) & 0x1 ));
-        printf("SSE2:    %d\n", ((cpeinfo >> 26) & 0x1 ));
-        printf("SSE3:    %d\n", ((cpsse       ) & 0x1 ));
-        printf("SSE4.1:  %d\n", ((cpsse >> 19) & 0x1 ));
-        printf("SSE4.2:  %d\n", ((cpsse >> 20) & 0x1 ));
-        printf("SSEAVX:  %d\n", ((cpsse >> 28) & 0x1 ));
+        printf ("CPU has the following instruction set features enabled: " );
+        for (i = 0; features[i].feature_name != NULL; ++i) {
+                if (cpu_has_feature (features[i].feature_code)) {
+                        printf ("%s ", features[i].feature_name);
+                }
+        }
+        printf ("\nCPU is missing the following instruction set features:  ");
+        for (i = 0; features[i].feature_name != NULL; ++i) {
+                if (! cpu_has_feature (features[i].feature_code)) {
+                        printf ("%s ", features[i].feature_name);
+                }
+        }
+        printf ("\n");
 }
